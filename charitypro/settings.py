e776d0c6abf6dc5a334578f6c7f529060be6f438
B-Mohammed2 <118529003+B-Mohammed2@users.noreply.github.com>
2023-09-16 21:00:30 +0100

    adding login page and css file

--- charitypro/settings.py
+++ charitypro/settings.py
@@ -151,7 +151,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/' # The URL prefix for static files.
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # The filesystem path where static files are stored.
+
+MEDIA_URL = '/media/'  # The URL prefix for media files.
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]  # The filesystem path where media files are stored.
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.index, name='home')
-    
+    path('', views.index, name='home'),
+    path('account_login/', auth_views.LoginView.as_view(), name='login'),
+    # path('account/signup/', auth_views.SignupView.as_view(), name='account_signup'),
+    # path('account/login/', auth_views.LoginView.as_view(), name='account_login'), 
 ]
+
