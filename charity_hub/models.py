8350d49d7070ce8cfdf235c7139a3b05d9c3fdd8
B-Mohammed2 <118529003+B-Mohammed2@users.noreply.github.com>
2023-09-17 18:51:24 +0100

    setup views

--- charity_hub/models.py
+++ charity_hub/models.py
@@ -16,6 +16,8 @@ class Charity(models.Model):
     contact_phone = models.CharField(max_length=20, null=True, blank=True)
     website = models.URLField(null=True, blank=True)
     donation_link = models.URLField(null=True, blank=True)
+    image_url = models.URLField (max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -28,6 +30,9 @@ class UserProfile(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     phone_number = models.CharField(max_length=15, blank=True)
     email = models.EmailField(max_length=100, blank=True)
+    
+    
+
 
     def __str__(self):
         return self.user.username
